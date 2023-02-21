ef627994c683e6faba5e7ece549bcba4e6d0d659
Oleksandr Zelko <zelkoalex@gmail.com>
2023-02-21 13:58:31 +0200

    Fixed code smells

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -17,7 +17,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 //import org.springframework.test.context.junit4.SpringRunner;  
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 //@RunWith(SpringRunner.class)
 @ExtendWith(SpringExtension.class)
