d8f18ccac8b16a5234d3228cda3c98736aeb1e3b
Oleksandr Zelko <zelkoalex@gmail.com>
2023-02-21 15:19:20 +0200

    Update NumericApplicationTests.java

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -8,15 +8,12 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//import org.junit.jupiter.api.Test;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-//import org.springframework.test.context.junit4.SpringRunner;  
 //import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 //@RunWith(SpringRunner.class)
